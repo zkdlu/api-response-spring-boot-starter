e6c747c931fe4aae89db8829848305a8ac5bfb8f
zkdlu <zkdlu159951@gmail.com>
2021-03-16 21:44:06 +0900

    response advice

---
+++ src/main/java/com/zkdlu/apiresponsespringbootstarter/interceptor/ResponseAdvice.java
@@ -0,0 +1,30 @@
+package com.zkdlu.apiresponsespringbootstarter.interceptor;
+
+import com.zkdlu.apiresponsespringbootstarter.service.ResponseService;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice
+public class ResponseAdvice implements ResponseBodyAdvice<Object> {
+    private final ResponseService responseService;
+
+    public ResponseAdvice(ResponseService responseService) {
+        this.responseService = responseService;
+    }
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        return responseService.getResult(body);
+    }
+}
