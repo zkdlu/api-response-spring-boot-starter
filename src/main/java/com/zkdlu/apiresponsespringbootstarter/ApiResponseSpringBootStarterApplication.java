13e95f23a8a0b4f7b03a5eaab765f5e109e4f7be
zkdlu <zkdlu159951@gmail.com>
2021-03-16 20:55:35 +0900

    rest api response service

--- src/main/java/com/zkdlu/apiresponsespringbootstarter/ApiResponseSpringBootStarterApplication.java
+++ src/main/java/com/zkdlu/apiresponsespringbootstarter/ApiResponseSpringBootStarterApplication.java
@@ -1,13 +1,38 @@
 package com.zkdlu.apiresponsespringbootstarter;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.zkdlu.apiresponsespringbootstarter.model.ListResult;
+import com.zkdlu.apiresponsespringbootstarter.service.ResponseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
-public class ApiResponseSpringBootStarterApplication {
+public class ApiResponseSpringBootStarterApplication implements ApplicationRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiResponseSpringBootStarterApplication.class, args);
     }
 
+    @Autowired
+    ResponseService responseService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        List<String> list = new ArrayList<>();
+        String hello = "hello";
+
+        list.add(hello);
+
+        ListResult<String> result = (ListResult<String>)responseService.getResult(list);
+        for (var i : result.getList()) {
+            System.out.println(i);
+        }
+    }
 }
--- src/main/java/com/zkdlu/apiresponsespringbootstarter/service/ResponseService.java
+++ src/main/java/com/zkdlu/apiresponsespringbootstarter/service/ResponseService.java
@@ -1,11 +1,32 @@
 package com.zkdlu.apiresponsespringbootstarter.service;
 
 import com.zkdlu.apiresponsespringbootstarter.model.CommonResult;
+import com.zkdlu.apiresponsespringbootstarter.model.ListResult;
+import com.zkdlu.apiresponsespringbootstarter.model.SingleResult;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
 
 @Service
 public class ResponseService {
     public <T> CommonResult getResult(T data) {
-        return null;
+        if (data instanceof Collection) {
+            return getListResult((List)data);
+        } else {
+            return getSingleResult(data);
+        }
+    }
+
+    private <T> SingleResult<T> getSingleResult(T data) {
+        SingleResult<T> result = new SingleResult<>();
+        result.setData(data);
+        return result;
+    }
+
+    private <T> ListResult<T> getListResult(List<T> list) {
+        ListResult<T> result = new ListResult<>();
+        result.setList(list);
+        return result;
     }
 }
