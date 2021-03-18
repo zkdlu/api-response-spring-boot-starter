10745901efb1e3ff0d452e56f8a8a774ca197c02
zkdlu <zkdlu159951@gmail.com>
2021-03-18 22:59:06 +0900

    왜 안될까?

--- src/main/java/com/zkdlu/apiresponsespringbootstarter/TestAutoConfiguration.java
+++ src/main/java/com/zkdlu/apiresponsespringbootstarter/TestAutoConfiguration.java
@@ -1,10 +1,12 @@
 package com.zkdlu.apiresponsespringbootstarter;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnClass(TestProperties.class)
 @EnableConfigurationProperties(TestProperties.class)
 public class TestAutoConfiguration {
     @Autowired
