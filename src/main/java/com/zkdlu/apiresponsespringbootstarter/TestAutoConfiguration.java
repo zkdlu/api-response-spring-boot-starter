a17e421e7e2f7b1c8b65d9c0d033bebbfbd98d40
zkdlu <zkdlu159951@gmail.com>
2021-03-18 23:17:59 +0900

    test

--- src/main/java/com/zkdlu/apiresponsespringbootstarter/TestAutoConfiguration.java
+++ src/main/java/com/zkdlu/apiresponsespringbootstarter/TestAutoConfiguration.java
@@ -3,12 +3,20 @@ package com.zkdlu.apiresponsespringbootstarter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConditionalOnClass(Test.class)
 @EnableConfigurationProperties(TestProperties.class)
 public class TestAutoConfiguration {
+    @Autowired
+    private TestProperties testProperties;
 
-
+    @Bean
+    public Test test() {
+        Test test = new Test();
+        test.testProperties = testProperties;
+        return test;
+    }
 }
