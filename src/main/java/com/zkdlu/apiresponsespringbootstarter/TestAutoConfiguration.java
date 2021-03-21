5717fc557c97617fbb735b51fbe44a974473857a
zkdlu <zkdlu159951@gmail.com>
2021-03-21 14:49:56 +0900

    import advice

--- src/main/java/com/zkdlu/apiresponsespringbootstarter/TestAutoConfiguration.java
+++ src/main/java/com/zkdlu/apiresponsespringbootstarter/TestAutoConfiguration.java
@@ -5,19 +5,15 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ConditionalOnClass(Test.class)
+@Import(ResponseAdvice.class)
 @EnableConfigurationProperties(TestProperties.class)
 public class TestAutoConfiguration {
-    @Autowired
-    private TestProperties testProperties;
-
     @Bean
     public Test test() {
-        Test test = new Test();
-        test.testProperties = testProperties;
-        return test;
+        return new Test();
     }
-
 }
