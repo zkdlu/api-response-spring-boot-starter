237ffe3b987adcd1cf8e2df0d1b32331b47eabcb
zkdlu <zkdlu159951@gmail.com>
2021-03-21 16:00:19 +0900

    fix

--- src/main/java/com/zkdlu/apiresponsespringbootstarter/TestAutoConfiguration.java
+++ src/main/java/com/zkdlu/apiresponsespringbootstarter/TestAutoConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Import;
 
 @Configuration
 @ConditionalOnClass(Test.class)
-@Import({ResponseAdvice.class, WebConfig.class})
 @EnableConfigurationProperties(TestProperties.class)
 public class TestAutoConfiguration {
 
