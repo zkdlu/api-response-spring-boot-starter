a80c0b2fe8e7403b15c373a307e57dbbded5041b
zkdlu <zkdlu159951@gmail.com>
2021-03-21 17:19:30 +0900

    update

--- src/main/java/com/zkdlu/apiresponsespringbootstarter/autoconfig/ResponseAutoConfiguration.java
+++ src/main/java/com/zkdlu/apiresponsespringbootstarter/autoconfig/ResponseAutoConfiguration.java
@@ -2,6 +2,7 @@ package com.zkdlu.apiresponsespringbootstarter.autoconfig;
 
 import com.zkdlu.apiresponsespringbootstarter.core.advice.ExceptionAdvice;
 import com.zkdlu.apiresponsespringbootstarter.core.advice.ResponseAdvice;
+import com.zkdlu.apiresponsespringbootstarter.core.config.WebConfig;
 import com.zkdlu.apiresponsespringbootstarter.core.service.ResponseService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Import;
 
 @Configuration
 @ConditionalOnClass(ResponseService.class)
-@Import({ExceptionAdvice.class, ResponseAdvice.class})
+@Import({ExceptionAdvice.class, ResponseAdvice.class, WebConfig.class})
 @EnableConfigurationProperties(ResponseProperties.class)
 public class ResponseAutoConfiguration {
 }
