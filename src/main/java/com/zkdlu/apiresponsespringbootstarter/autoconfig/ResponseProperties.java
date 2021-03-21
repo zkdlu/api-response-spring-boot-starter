4b1c297bddeafb5392d8e8214a7ab2918be5c255
zkdlu <zkdlu159951@gmail.com>
2021-03-21 17:26:42 +0900

    i want list properties

--- src/main/java/com/zkdlu/apiresponsespringbootstarter/autoconfig/ResponseProperties.java
+++ src/main/java/com/zkdlu/apiresponsespringbootstarter/autoconfig/ResponseProperties.java
@@ -1,13 +1,17 @@
 package com.zkdlu.apiresponsespringbootstarter.autoconfig;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @ConfigurationProperties("spring.response")
 public class ResponseProperties {
-    private List<ExceptionModel> exceptions;
+    private List<ExceptionModel> exceptions = new ArrayList<>();
 
+    @Bean
+    @ConfigurationProperties("exceptions")
     public List<ExceptionModel> getExceptions() {
         return exceptions;
     }
