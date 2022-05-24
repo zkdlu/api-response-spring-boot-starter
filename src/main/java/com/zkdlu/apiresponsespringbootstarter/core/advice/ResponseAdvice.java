1020c1408b17e83770eba3e67ec116e514465b29
PCloud <pcloud63514@gmail.com>
2022-05-24 10:18:42 +0900

    Swagger 호환성을 위해 변경사항을 추가하였습니다. properties로 제외하여 설정해도 좋을 것 같습니다.

--- src/main/java/com/zkdlu/apiresponsespringbootstarter/core/advice/ResponseAdvice.java
+++ src/main/java/com/zkdlu/apiresponsespringbootstarter/core/advice/ResponseAdvice.java
@@ -7,13 +7,26 @@ import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.PathContainer;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+import org.springframework.web.util.pattern.PathPattern;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestControllerAdvice
 public class ResponseAdvice implements ResponseBodyAdvice<Object> {
+    private final List<PathPattern> whitelist = Arrays.asList(
+            new PathPatternParser().parse("/v*/api-docs"),
+            new PathPatternParser().parse("/swagger-resources/**"),
+            new PathPatternParser().parse("/swagger-ui.html"),
+            new PathPatternParser().parse("/webjars/**"),
+            new PathPatternParser().parse("/swagger/**"));
+
     private final ResponseService responseService;
     private final ResponseProperties responseProperties;
 
@@ -29,6 +42,7 @@ public class ResponseAdvice implements ResponseBodyAdvice<Object> {
 
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        if (whitelist.stream().anyMatch(pathPattern -> pathPattern.matches(PathContainer.parsePath(request.getURI().getPath())))) return body;
         if (body instanceof CommonResult) {
             CommonResult commonResult = (CommonResult) body;
             try {
